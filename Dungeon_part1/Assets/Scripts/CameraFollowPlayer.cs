0608f26f31a2721e59b7c372d7e2a2a4e63d9382
Weiwei Wan <wwan@tcd.ie>
2024-07-31 12:06:29 +0100

    添加相机范围

--- Dungeon_part1/Assets/Scripts/CameraFollowPlayer.cs
+++ Dungeon_part1/Assets/Scripts/CameraFollowPlayer.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    public static Scene currentScene;
+    public static string sceneName;
+
+    void Awake()
     {
-        
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        switch (sceneName)
+        {
+            case "dungeon_elevator":
+                if (player.transform.position.x > 0.5f) {
+                    transform.position = new Vector3(0.5f, transform.position.y, transform.position.z);
+                } else if (player.transform.position.x < -4) {
+                    transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+                } else {
+                    transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+                }
+                break;
+            default:
+                transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+                break;
+        }
     }
 }
