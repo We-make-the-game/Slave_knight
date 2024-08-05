2acf87e503265e10baf9f37d5eeb52bdb3e3d2be
Weiwei Wan <wwan@tcd.ie>
2024-08-05 02:37:20 +0100

    finish scene switch and player's initial position in each scene

--- Dungeon_part1/Assets/Scripts/CameraFollowPlayer.cs
+++ Dungeon_part1/Assets/Scripts/CameraFollowPlayer.cs
@@ -8,31 +8,158 @@ public class CameraFollowPlayer : MonoBehaviour
     public GameObject player;
     public static Scene currentScene;
     public static string sceneName;
+    string previousSceneName;
 
     void Awake()
     {
+        previousSceneName = sceneName;
         currentScene = SceneManager.GetActiveScene();
         sceneName = currentScene.name;
+        if (previousSceneName == null) {
+            previousSceneName = "None";
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        GetPlayerTransform();
+    }
+
     void Update()
+    {
+        UpdateCameraPos();
+    }
+
+    void GetPlayerTransform()
+    {
+        Debug.Log("previousSceneName: " + previousSceneName);
+        Debug.Log("currentSceneName: " + sceneName);
+        switch (sceneName)
+        {
+            case "dungeon_elevator":
+                if (previousSceneName == "dungeon_underground") {
+                    player.transform.position = new Vector3(-9.4f, -2.3f, -1.5f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    transform.position = new Vector3(transform.position.x, 2, transform.position.z);
+                } else if (previousSceneName == "dungeon_ground") {
+                    player.transform.position = new Vector3(-9.4f, 8.2f, -1.5f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    transform.position = new Vector3(transform.position.x, 12.5f, transform.position.z);
+                }
+                break;
+            case "dungeon_cell":
+                if (previousSceneName == "None") {
+                    player.transform.position = new Vector3(-5, -1.81f, -3.37f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "dungeon_underground") {
+                    player.transform.position = new Vector3(6.5f, -1.81f, -3.37f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "dungeon_underground":
+                if (previousSceneName == "dungeon_cell") {
+                    player.transform.position = new Vector3(-7, -2.3f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "dungeon_elevator") {
+                    player.transform.position = new Vector3(30.6f, -2.3f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "dungeon_ground":
+                if (previousSceneName == "dungeon_corner_room") {
+                    player.transform.position = new Vector3(-9, -2.3f, -1.5f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "dungeon_elevator") {
+                    player.transform.position = new Vector3(59.33f, -2.3f, -1.5f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "castle_courtyard":
+                if (previousSceneName == "dungeon_corner_room") {
+                    player.transform.position = new Vector3(-10.5f, -2.6f, -3.21f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "castle_entry") {
+                    player.transform.position = new Vector3(60.5f, -3.33f, -3.21f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "castle_entry":
+                if (previousSceneName == "castle_courtyard") {
+                    player.transform.position = new Vector3(-2.2f, -3.3f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "dungeon_bridge") {
+                    player.transform.position = new Vector3(26.5f, -3.3f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "dungeon_bridge":
+                if (previousSceneName == "castle_entry") {
+                    player.transform.position = new Vector3(4, -9, 38.44f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                }
+                break;
+            case "dungeon_corner_room":
+                if (previousSceneName == "castle_courtyard") {
+                    player.transform.position = new Vector3(0.2f, -1.85f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else if (previousSceneName == "dungeon_ground") {
+                    player.transform.position = new Vector3(6, -1.85f, -2.19f);
+                    player.transform.rotation = Quaternion.Euler(0, -90, 0);
+                }
+                break;
+            case "dungeon_guardian_room":
+                break;
+            default:
+                break;
+        }
+    }
+
+    void UpdateCameraPos()
     {
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         switch (sceneName)
         {
             case "dungeon_elevator":
-                if (player.transform.position.x > 0.5f) {
-                    transform.position = new Vector3(0.5f, transform.position.y, transform.position.z);
-                } else if (player.transform.position.x < -4) {
-                    transform.position = new Vector3(-4, transform.position.y, transform.position.z);
-                } else {
-                    transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-                }
+                transform.position = LimitCameraPos(-4, 0.5f, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_cell":
+                transform.position = LimitCameraPos(-2.5f, -0.5f, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_underground":
+                transform.position = LimitCameraPos(-2.5f, 25.5f, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_ground":
+                transform.position = LimitCameraPos(-4, 54.5f, player.transform.position.x, transform.position);
+                break;
+            case "castle_courtyard":
+                transform.position = LimitCameraPos(-1.6f, 51, player.transform.position.x, transform.position);
+                break;
+            case "castle_entry":
+                transform.position = LimitCameraPos(7.6f, 26, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_bridge":
+                transform.position = LimitCameraPos(8, 35, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_corner_room":
+                transform.position = LimitCameraPos(-18, 1, player.transform.position.x, transform.position);
+                break;
+            case "dungeon_guardian_room":
+                transform.position = LimitCameraPos(-24, -6, player.transform.position.x, transform.position);
                 break;
             default:
                 transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
                 break;
+        }
+    }
+
+    Vector3 LimitCameraPos(float x_l, float x_r, float player_x, Vector3 cameraPos) 
+    {
+        if (cameraPos.x > x_r) {
+            return new Vector3(x_r, cameraPos.y, cameraPos.z);
+        } else if (cameraPos.x < x_l) {
+            return new Vector3(x_l, cameraPos.y, cameraPos.z);
+        } else {
+            return new Vector3(player_x, cameraPos.y, cameraPos.z);
         }
     }
 }
--- Dungeon_part1/Assets/Scripts/SceneSwitcher.cs
+++ Dungeon_part1/Assets/Scripts/SceneSwitcher.cs
@@ -3,13 +3,82 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public string sceneNameToLoad;
+    GameObject player;
+    string sceneName;
 
-    void OnTriggerEnter(Collider player)
+    void Start()
     {
-        if (player.CompareTag("Player"))
+        player = GameObject.Find("player");
+        sceneName = CameraFollowPlayer.sceneName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         switch (sceneName)
         {
-            SceneManager.LoadScene(sceneNameToLoad);
+            case "dungeon_elevator":
+            if (player.transform.position.x < -10) {
+                if (player.transform.position.y > 0) {
+                    SceneManager.LoadScene("dungeon_ground");
+                } else {
+                    SceneManager.LoadScene("dungeon_underground");
+                }
+            }
+                break;
+            case "dungeon_cell":
+                if (player.transform.position.x > 7) {
+                    SceneManager.LoadScene("dungeon_underground");
+                }
+                break;
+            case "dungeon_underground":
+                if (player.transform.position.x > -8 && player.transform.position.x < -6.2f) {
+                    if (Input.GetKeyDown(KeyCode.E)) {
+                        SceneManager.LoadScene("dungeon_cell");
+                    }
+                } else if (player.transform.position.x > 31) {
+                    SceneManager.LoadScene("dungeon_elevator");
+                }
+                break;
+            case "dungeon_ground":
+                if (player.transform.position.x > 60) {
+                    SceneManager.LoadScene("dungeon_elevator");
+                } else if (player.transform.position.x < -9.5f) {
+                    SceneManager.LoadScene("dungeon_corner_room");
+                }
+                break;
+            case "castle_courtyard":
+                if (player.transform.position.x > -11.5f && player.transform.position.x < -9.5f) {
+                    if (Input.GetKeyDown(KeyCode.E)) {
+                        SceneManager.LoadScene("dungeon_corner_room");
+                    }
+                } else if (player.transform.position.x > 61) {
+                    SceneManager.LoadScene("castle_entry");
+                }
+                break;
+            case "castle_entry":
+                if (player.transform.position.x > 24 && player.transform.position.x < 28.5f) {
+                    if (Input.GetKeyDown(KeyCode.E)) {
+                        SceneManager.LoadScene("dungeon_bridge");
+                    }
+                } else if (player.transform.position.x < -3) {
+                    SceneManager.LoadScene("castle_courtyard");
+                }
+                break;
+            case "dungeon_bridge":
+                if (player.transform.position.x < 3.5f) {
+                    SceneManager.LoadScene("castle_entry");
+                }
+                break;
+            case "dungeon_corner_room":
+                if (player.transform.position.x > 6.5f) {
+                    SceneManager.LoadScene("dungeon_ground");
+                }
+                break;
+            case "dungeon_guardian_room":
+                break;
+            default:
+                break;
         }
     }
 }
